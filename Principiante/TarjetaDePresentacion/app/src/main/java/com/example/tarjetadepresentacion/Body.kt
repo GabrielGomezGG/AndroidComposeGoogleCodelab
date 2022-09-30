3bcc4f5ff62d95da91b36c3edac9b37d07b7f6e4
GabrielGG <gabrielgomezgg1997@gmail.com>
2022-09-29 21:14:43 -0300

    Body terminado

---
+++ Principiante/TarjetaDePresentacion/app/src/main/java/com/example/tarjetadepresentacion/Body.kt
@@ -0,0 +1,30 @@
+package com.example.tarjetadepresentacion
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun MyBody(modifier: Modifier = Modifier) {
+    Column(modifier = modifier
+        .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.android_logo),
+            contentDescription = "logo",
+            Modifier.size(114.dp)
+        )
+        Spacer(modifier = Modifier.size(6.dp))
+        Text(text = "Gabriel Gomez", fontSize = 50.sp, color = Color.White)
+        Text(text = "Programador backend", color = Color(0xFF48dc9a))
+    }
+}
--- Principiante/TarjetaDePresentacion/app/src/main/java/com/example/tarjetadepresentacion/BussinessCard.kt
+++ Principiante/TarjetaDePresentacion/app/src/main/java/com/example/tarjetadepresentacion/BussinessCard.kt
@@ -1,8 +1,20 @@
 package com.example.tarjetadepresentacion
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 
+@Preview(showSystemUi = true)
 @Composable
-fun MyBusinessCard(){
-
+fun MyBusinessCard() {
+    Column(
+        Modifier
+            .fillMaxSize()
+            .background(Color(0xFF003a4a))) {
+        MyBody(Modifier)
+    }
 }
